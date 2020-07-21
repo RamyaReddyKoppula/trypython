4bbf76ed3ed83dd01783c02f1274bc8f0bb58222
RamyaReddyKoppula <ramyasrireddy28@gmail.com>
2020-07-21 16:51:43 +0200

    third type

---
+++ code.py
@@ -0,0 +1,9 @@
+import math
+print("enter the radius of circle")
+radius = int(input())
+circumference_of_circle = 2*math.pi*radius
+area_of_the_circle = math.pi*(radius**2)
+expo = math.exp(radius)
+print("circumference of circle is", circumference_of_circle)
+print("area of the circle is", area_of_the_circle)
+print(expo)
